2a5e4a179e04d3578e23aa164346813822efbfcf
Your Name <you@example.com>
2020-09-25 09:06:25 +0000

    Comples hello

--- hello.c
+++ hello.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-int main(int argc, char *argv[]) {
-    printf("Hello, World!\n");
+#define nE4ATb printf
+
+int main(int argc, char *argv[], char *envp[]) {
+
+    char **e;
+    int i;
+    for(e=envp, i=0; *e; e++, i++)
+	    nE4ATb("%d: %s\n", i, *e);
+
     return 0;
 }
